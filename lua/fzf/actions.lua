90043efefda353b56ee8f0ba72a232d1ccb4d6ff
Lukas Reineke <lukas.reineke@protonmail.com>
2021-01-05 17:57:40 +0900

    resolve nvim_fzf_directory when needed
    The lua part of the plugin might get loaded first, in which case the
    local `nvim_fzf_directory` variable would be nil

--- lua/fzf/actions.lua
+++ lua/fzf/actions.lua
@@ -1,13 +1,12 @@
 local registry = require("fzf.registry")
 
-local nvim_fzf_directory = vim.g.nvim_fzf_directory
-local shell_script_path = string.format("%s/action_helper.sh", nvim_fzf_directory)
-
 local function escape(str)
   return vim.fn.shellescape(str)
 end
 
 local function raw_action(fn)
+  local nvim_fzf_directory = vim.g.nvim_fzf_directory
+  local shell_script_path = string.format("%s/action_helper.sh", nvim_fzf_directory)
   local id = registry.register_func(fn)
   local action_string = string.format("%s %s %s {+}",
     escape(shell_script_path), escape(nvim_fzf_directory), id)
