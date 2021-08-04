eb708714c2a739f2d29f63f706d1e4e1095958c8
bhagwan <bhagwan@disroot.org>
2021-08-03 23:38:46 -0700

    finer control on action|raw_action filespec

--- lua/fzf/actions.lua
+++ lua/fzf/actions.lua
@@ -6,17 +6,18 @@ local function escape(str)
   return vim.fn.shellescape(str)
 end
 
-function M.raw_action(fn)
+function M.raw_action(fn, filespec)
   local nvim_fzf_directory = vim.g.nvim_fzf_directory
-  local shell_script_path = string.format("%s/action_helper.sh", nvim_fzf_directory)
   local id = registry.register_func(fn)
-  local action_string = string.format("%s %s %s {+}",
-    escape(shell_script_path), escape(nvim_fzf_directory), id)
+  local action_string = string.format("nvim --headless --clean --cmd %s %s %s %s",
+    vim.fn.shellescape("luafile " .. nvim_fzf_directory .. "/action_helper.lua"),
+    vim.fn.shellescape(nvim_fzf_directory),
+    id, filespec or "{+}")
   return action_string, id
 end
 
-function M.action(fn)
-  local action_string, id = M.raw_action(fn)
+function M.action(fn, filespec)
+  local action_string, id = M.raw_action(fn, filespec)
   return escape(action_string), id
 end
 
