58e8fea7ee37a671dcd5b514b5f9b2c6eac85e2c
Vijay Marupudi <vijay@vijaymarupudi.com>
2021-10-25 16:39:37 -0400

    Moved WriteQueue to it's own file for reuse

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -1,5 +1,6 @@
 local uv = vim.loop
 local float = require('fzf.floating_window')
+local WriteQueue = require("fzf.utils").WriteQueue
 
 local FZF = {}
 
@@ -104,54 +105,6 @@ local function generate_fzf_command(opts, contents)
 
   command = command .. " > " .. vim.fn.shellescape(outputtmpname)
   return command, fifotmpname, outputtmpname, cwd
-end
-
-local WriteQueue = {}
-WriteQueue.__index = WriteQueue
-
-function WriteQueue:new(output_pipe)
-  local q = {
-    done_state = false,
-    output_pipe = output_pipe,
-    n_enqueued = 0,
-    close_when_done_flag = false
-  }
-  setmetatable(q, self)
-  return q
-end
-
-function WriteQueue:close()
-  if not self.done_state then
-    self.done_state = true
-    self.output_pipe:close()
-  end
-end
-
-function WriteQueue:close_when_done()
-  self.close_when_done_flag = true
-  if self.n_enqueued == 0 then
-    self:close()
-  end
-end
-
-function WriteQueue:enqueue(input, cb)
-  if self.done_state then
-    if cb then cb("PIPE closed") end
-    return nil
-  end
-  self.n_enqueued = self.n_enqueued + 1
-  self.output_pipe:write(input, function(err)
-    if err then
-      self:close()
-      if cb then cb(err) end
-      return nil
-    end
-    self.n_enqueued = self.n_enqueued - 1
-    if self.n_enqueued == 0 and self.close_when_done_flag then
-      self:close()
-    end
-    if cb then cb(nil) end
-  end)
 end
 
 local FZFObject = {}
