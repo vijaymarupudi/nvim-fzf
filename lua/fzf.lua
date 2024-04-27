1f55c1d5cb341c06e5f0a30d897e1320d7940c34
Song-Tianxiang <149415622+Song-Tianxiang@users.noreply.github.com>
2024-04-27 14:40:31 +0800

    if fzf_cmd_args not nil, add space before and after to fzf_cmd_args

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -287,6 +287,9 @@ function FZF.raw_fzf(contents, fzf_cli_args, user_options)
    error("please run function in a coroutine")
  end
   local co = coroutine.running()
+  if fzf_cli_args then
+    fzf_cli_args = ' ' .. fzf_cli_args .. ' '
+  end
   local fzf_obj = FZFObject:new(contents, fzf_cli_args, user_options, function(ret, exit_code)
     coroutine.resume(co, ret, exit_code)
   end)
